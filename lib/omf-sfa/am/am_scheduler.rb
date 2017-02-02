19010fd8099a6f6250f019f7852970f0bb7b5d2c
Vinícius Gonçalves Braga <vinicius.esufg@gmail.com>
2017-02-02 17:15:23 -0200

    refectoring and documentation

--- lib/omf-sfa/am/am_scheduler.rb
+++ lib/omf-sfa/am/am_scheduler.rb
@@ -270,6 +270,13 @@ module OMF::SFA::AM
       leases.nil? || leases.empty?
     end
 
+    # Check if a parent component has enough resources available to support a sliver component at a certain time slot.
+    #
+    # @param [OMF::SFA::Model::Lease] an array of leases to the parent component configured at the same time in which the user wants to create a lease to the sliver component.
+    # @param [OMF::SFA::Model::Component] the parent compoment.
+    # @param [OMF::SFA::Model::Component] the component with the sliver_type configuration.
+    # @return [Boolean] true if the parent component has enough resources available, false if it has not.
+    #
     def sliver_available?(leases, parent, component)
       allocated_cpu_cores = 0
       allocated_ram = 0
@@ -286,10 +293,7 @@ module OMF::SFA::AM
       total_cpu_cores = allocated_cpu_cores + component.sliver_type.cpu_cores
       total_ram = allocated_ram + component.sliver_type.ram_in_mb
 
-      parent_cores = 0
-      parent.cpus.each {|cpu|
-        parent_cores += cpu.cores * cpu.threads
-      }
+      parent_cores = parent.cpus.inject(0) {|sum, cpu| sum + (cpu.cores * cpu.threads)}
 
       return total_cpu_cores <= parent_cores && total_ram <= parent.ram.to_i
     end
