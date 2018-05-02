c2e9bd5b724db072b5ed940b5c1618385473273f
Bruno Soares da Silva <brunosoares.inf@gmail.com>
2018-05-02 15:59:55 -0300

    - Forcing show hypervisor interfaces of VM hash return

--- lib/omf-sfa/models/node.rb
+++ lib/omf-sfa/models/node.rb
@@ -96,8 +96,10 @@ module OMF::SFA::Model
         value[:status] = self.sliver_type.status if self.sliver_type.status
         value[:ip_address] = self.sliver_type.ip_address if self.sliver_type.ip_address
         value[:mac_address] = self.sliver_type.mac_address if self.sliver_type.mac_address
-        if self.parent
-          parent = OMF::SFA::Model::Node.first({:uuid => self.parent.uuid})
+        value[:hypervisor_interfaces] = self.parent.interfaces if self.parent and self.parent.interfaces
+        unless self.parent and self.parent.interfaces
+          default_account_id = OMF::SFA::Model::Account.where(name: '__default__').first.id
+          parent = OMF::SFA::Model::Node.first({:urn => self.urn, :account_id => default_account_id})
           value[:hypervisor_interfaces] = parent.interfaces if parent
         end
       end
